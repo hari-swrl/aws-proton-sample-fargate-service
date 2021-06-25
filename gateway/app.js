f64b22f6c2aa29e37e4e492d234119e1765100f5
hareesh <hareesh@swrlio.com>
2021-06-25 22:26:19 +0530

    adding gateway and url changes

--- gateway/app.js
+++ gateway/app.js
@@ -6,12 +6,6 @@ app.get('/', async (req, res) => {
     let result = { }
     // http://be-api.example.local/
     try{
-    result.try1 = await axios.get("http://be-api.example.local:80/")
-    result.try1 =  result.try1.data
-    }catch(error){
-        result.try1=error.toString() 
-    }
-    try{
     result.try2 = await axios.get("http://be-api.example.local:3000/")
     result.try2 =  result.try2.data
     }catch(error){
@@ -23,6 +17,15 @@ app.get('/', async (req, res) => {
     }catch(error){
         result.try3=error.toString() 
     }
+    try{
+        result.try4 = await axios.get("http://backendapi_be-api.be-api.example.local/")
+       result.try4 =  result.try4.data
+       }catch(error){
+           result.try4=error.toString() 
+       }
+       console.log(prcess.env)
     res.json({message: 'From gateway', ...result})
+
+    // backendapi_be-api
 })
 app.listen(80, () => console.log('Server ready'))
