3fbb95c8374f24bcf3bba8619c295ef1331a8f09
hareesh <hareesh@swrlio.com>
2021-06-25 17:52:01 +0530

    adding gateway and url changes

--- backendApi/app.js
+++ backendApi/app.js
@@ -2,4 +2,4 @@ const express = require('express')
 const app = express()
 
 app.get('/', (req, res) => res.json({message: 'Hello from micro!'}))
-app.listen(3000, () => console.log('Server ready'))
+app.listen(80, () => console.log('Server ready'))
